78602ab26f5c50fff8f36d4f0aa887226a738dbe
Ana-KHS <ana.hskwon@gmail.com>
2023-03-25 11:06:32 +0900

    Initial commit

---
+++ src/main/java/com/sqisoft/tarottarot/TarottarotApplication.java
@@ -0,0 +1,13 @@
+package com.sqisoft.tarottarot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarottarotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarottarotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sqisoft/tarottarot/TarottarotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqisoft.tarottarot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarottarotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
